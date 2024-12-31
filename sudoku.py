fc3dd18f374e4aaec38dda8994240147c437c8e6
radhikagodbole <radhikagodbole4@gmail.com>
2024-12-31 19:53:39 +0530

    changed the sudoku.py file for csv input

--- sudoku.py
+++ sudoku.py
@@ -1,21 +1,21 @@
+import csv
 from z3 import Bool, Solver, Or, And, sat, is_true
 
 # Create boolean variables
-# p = Bool('p')
-# q = Bool('q')
 BIJK = [[[Bool(f'b_{i}{j}{k}') for k in range(9)] for j in range(9)] for i in range(9)]
 
-# input_array = [[-1 for i in range(9)] for j in range(9)]
-input_array = []
-for i in range(9):
-    row = []
-    for j in range(9):
-        value = int(input(f"Enter value for ({i+1},{j+1}) : "))
-        row.append(value)
-    input_array.append(row)
+# Function to read the Sudoku grid from a CSV file
+def read_sudoku_from_csv(file_path):
+    input_array = []
+    with open(file_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            # Parse each row and convert to integers, use -1 for empty cells
+            input_array.append([int(val) if val else -1 for val in row])
+    return input_array
 
-# Sanity check
-# print(BIJK[0][0][0])
+# Read the Sudoku grid from the CSV file
+input_array = read_sudoku_from_csv('sudoku.csv')
 
 # Create a solver instance
 solver = Solver()
@@ -25,8 +25,8 @@ all_constraints = []
 # Add existing numbers
 for i in range(9):
     for j in range(9):
-        if input_array[i][j] != -1:    
-            all_constraints.append(BIJK[i][j][input_array[i][j]-1])
+        if input_array[i][j] != -1:
+            all_constraints.append(BIJK[i][j][input_array[i][j] - 1])
 
 # Add row constraints
 for i in range(9):
@@ -42,16 +42,11 @@ for j in range(9):
 
 # Add square constraints
 for sq in range(9):
-    # sq, col_offset, row_offset
-        # 0, 0, 0
-        # 1, 1, 0
-        # 2, 2, 0
     col_offset = sq % 3
     row_offset = int(sq / 3)
-    # print(col_offset, row_offset)
     for k in range(9):
         indices = [
-            (row_offset*3+_i, col_offset*3+_j)
+            (row_offset * 3 + _i, col_offset * 3 + _j)
             for _i in range(3) for _j in range(3)
         ]
         cell_list = [BIJK[_i][_j][k] for (_i, _j) in indices]
@@ -67,21 +62,16 @@ for i in range(9):
                 cell_constraint = ~And(BIJK[i][j][k1], BIJK[i][j][k2])
                 all_constraints.append(cell_constraint)
 
-
-# Add a constraint (e.g., p AND q)
-# constraint = p & ~p
+# Add a constraint
 solver.add(And(all_constraints))
 
 if solver.check() == sat:
     m = solver.model()
     values = [[[m.evaluate(BIJK[i][j][k]) for k in range(9)] 
                for j in range(9)] for i in range(9)]
-    # print(values)
     correct_values = [
-        [values[i][j].index(True)+1 for j in range(9)]
+        [values[i][j].index(True) + 1 for j in range(9)]
             for i in range(9)
     ]
-    # print(correct_values)
-
     for row in correct_values:
         print(" ".join(map(str, row)))
